9b97c08e1c19f75fb87ae0b84af715138cabb60f
Sergio Robles <sroblesc@gmail.com>
2020-01-10 13:54:05 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,36 @@ require 'pry'
 
 class String
 
+
+
   def sentence?
-    
+    if self[-1] == "."
+      true 
+    else 
+      false
+    end 
+  
   end
 
   def question?
+    if self[-1] == "?"
+      true 
+    else 
+      false 
+    end
 
   end
 
   def exclamation?
+    if self[-1] == "!"
+      true 
+    else 
+      false 
+    end
 
   end
 
   def count_sentences
-
+    self.scan(/[^\.!?]+[\.!?]/).map.count
   end
 end
