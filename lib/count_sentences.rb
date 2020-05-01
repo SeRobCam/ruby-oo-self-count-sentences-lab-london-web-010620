65221627c6236514dd4504afe84486bd26bd481d
Sergio Robles <sroblesc@gmail.com>
2020-05-01 13:15:41 +0100

    Saved

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -34,4 +34,7 @@ class String
   def count_sentences
     self.scan(/[^\.!?]+[\.!?]/).map.count
   end
+  #Scan method: The scan method returns an array of all items in your string that match a given Regular Expression. 
+  #Match method: The match method returns the first item in your string that matches a given Regular Expression as a MatchData object. 
+  #Grep method: Grep is an enumerable method for pattern searching in arrays and hashes. Similar to scan, grep will return an array of matching items from an array.
 end
